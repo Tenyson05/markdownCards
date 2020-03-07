bf3ce520eeaf9a7cb0f25378498ba6d5a99c904c
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-03-06 17:15:48 -0800

    Changed page data

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Enjoy your stay</p>
+    <p>Please go to the next page to see something nice.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
