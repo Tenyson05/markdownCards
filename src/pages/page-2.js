e284e5b647ff9e3e21015a4129e153d06a9e12b8
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-03-12 06:11:35 -0700

    removed

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,25 +1,46 @@
-import React from "react"
-import { Link } from "gatsby"
+// import React from "react"
+// import { Link } from "gatsby"
+// import { graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import picto from "../images/gatsby-icon.png"
+// import Layout from "../components/layout"
+// import SEO from "../components/seo"
+// import picto from "../images/gatsby-icon.png"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi, welcome to the fun page</h1>
-    <p>Welcome to page 2</p>
-	<div className="card-body">
-		<img src={ picto } className="pic"/>
-		<div className="card-text">
-			<h4 styles={{ display: "block" }}>Testing zone</h4>
-			<h5>ANother testing zone</h5>
-		</div>
-	</div>
+// export default function SecondPage({
+// 	data,
+// }) {
+// 	const { markdownRemark } = data // data.markdownRemark holds your post data
+// 	const { frontmatter, html } = markdownRemark
+// 	return (
+// 		<Layout>
+// 		<SEO title="Page two" />
+// 		<h1>Hi, welcome to the fun page</h1>
+// 		<p>Welcome to page 2</p>
+// 		<div className="card-body">
+// 			<img src={ picto } className="pic"/>
 
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+// 			<div className="card-text">
+// 				<h4 styles={{ display: "block" }}>{ frontmatter.title }</h4>
+// 				<h5>{ frontmatter.date }</h5>
+// 				dangerouslySetInnerHTML={{ __html: html }}
+// 			</div>
+// 		</div>
+	
+// 		<Link to="/">Go back to the homepage</Link>
+// 	  </Layout>
+// 	)
+// }
 
-export default SecondPage
+
+// export const test = graphql`
+//   query($path: String!) {
+//     allMarkdownRemark(frontmatter: { path: { eq: $path } }) {
+//       html
+//       frontmatter {
+//         date(formatString: "MMMM DD, YYYY")
+//         path
+//         title
+//       }
+//     }
+//   }
+// `
