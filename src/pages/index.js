2490edb7f229e4878d106b173de95cee326a47d2
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-03-12 06:11:19 -0700

    imported Ting data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Tings from "../components/pagetings"
 
 const IndexPage = () => (
   <Layout>
@@ -14,7 +15,10 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+	<div style={{ maxWidth: `auto`, maxHeight: 'auto'}}>
+		<Tings />
+	</div>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
